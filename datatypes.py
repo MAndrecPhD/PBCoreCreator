d7219c6c8214c9e88b098f9cefeec941acedfb6e
Michael Andrec <mandrecphd@gmail.com>
2016-11-17 18:48:37 -0500

    added stubs for datatypes

---
+++ datatypes.py
@@ -0,0 +1,19 @@
+class Pbcore_Description(None):
+    def __init__(self):
+        pass
+
+    def placeholder(self):
+        pass
+
+
+
+class Pbcore_Contributor(None):
+    pass
+
+
+class Pbcore_AnalogPremis(None):
+    pass
+
+
+class Pbcore_DigitalInst(None):
+    pass
