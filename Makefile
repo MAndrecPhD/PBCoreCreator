200354afe14688a641f8bff17495de2f5f36e227
Michael Andrec <mandrecphd@gmail.com>
2016-11-21 18:45:34 -0500

    removed proliferation of dialog boxes and replaced with a generic

--- Makefile
+++ Makefile
@@ -1,16 +1,10 @@
-all: mainwindow language description contributor analogpremis 
+all: mainwindow analogpremis generic
 
 mainwindow: mainwindow.ui
 	pyuic5 mainwindow.ui -o mainwindow.py
 
-language: language.ui
-	pyuic5 language.ui -o language.py
-
-description: description.ui
-	pyuic5 description.ui -o description.py
-
-contributor: contributor.ui
-	pyuic5 contributor.ui -o contributor.py
+generic: genericInputbox.ui
+	pyuic5 genericInputbox.ui -o genericInputbox.py
 
 analogpremis: analogpremis.ui
 	pyuic5 analogpremis.ui -o analogpremis.py
--- genericInputbox.py
+++ genericInputbox.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'description.ui'
+# Form implementation generated from reading ui file 'genericInputbox.ui'
 #
 # Created by: PyQt5 UI code generator 5.6
 #
@@ -11,22 +11,22 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_DescriptionDialog(object):
     def setupUi(self, DescriptionDialog):
         DescriptionDialog.setObjectName("DescriptionDialog")
-        DescriptionDialog.resize(290, 300)
+        DescriptionDialog.resize(326, 438)
         self.layoutWidget = QtWidgets.QWidget(DescriptionDialog)
-        self.layoutWidget.setGeometry(QtCore.QRect(10, 10, 268, 272))
+        self.layoutWidget.setGeometry(QtCore.QRect(10, 10, 301, 421))
         self.layoutWidget.setObjectName("layoutWidget")
         self.verticalLayout = QtWidgets.QVBoxLayout(self.layoutWidget)
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout.setObjectName("verticalLayout")
-        self.description_type = QtWidgets.QComboBox(self.layoutWidget)
-        self.description_type.setObjectName("description_type")
-        self.description_type.addItem("")
-        self.description_type.addItem("")
-        self.description_type.addItem("")
-        self.verticalLayout.addWidget(self.description_type)
-        self.description_text = QtWidgets.QPlainTextEdit(self.layoutWidget)
-        self.description_text.setObjectName("description_text")
-        self.verticalLayout.addWidget(self.description_text)
+        self.attribute = QtWidgets.QComboBox(self.layoutWidget)
+        self.attribute.setObjectName("attribute")
+        self.verticalLayout.addWidget(self.attribute)
+        self.text = QtWidgets.QPlainTextEdit(self.layoutWidget)
+        self.text.setObjectName("text")
+        self.verticalLayout.addWidget(self.text)
+        self.preset = QtWidgets.QComboBox(self.layoutWidget)
+        self.preset.setObjectName("preset")
+        self.verticalLayout.addWidget(self.preset)
         self.buttonBox = QtWidgets.QDialogButtonBox(self.layoutWidget)
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
         self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
@@ -41,7 +41,4 @@ class Ui_DescriptionDialog(object):
     def retranslateUi(self, DescriptionDialog):
         _translate = QtCore.QCoreApplication.translate
         DescriptionDialog.setWindowTitle(_translate("DescriptionDialog", "Description"))
-        self.description_type.setItemText(0, _translate("DescriptionDialog", "Description"))
-        self.description_type.setItemText(1, _translate("DescriptionDialog", "Abstract"))
-        self.description_type.setItemText(2, _translate("DescriptionDialog", "Playlist"))
 
